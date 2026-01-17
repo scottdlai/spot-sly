d60f2887df194e79f83a8c553a7c091802f66a82
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-01-17 14:54:29 -0800

    feat: highlight middle letter

--- src/reader/index.tsx
+++ src/reader/index.tsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
+import './index.css';
 
 const testText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat est est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec convallis, quam eget bibendum ultricies, nisl ante porta mi, rutrum posuere sem nisl vel est. Vivamus efficitur iaculis dignissim. Etiam egestas consectetur posuere. Aenean eu magna eu sapien mollis fringilla. Praesent est ligula, dignissim id malesuada vitae, consectetur eget arcu. Etiam a neque et justo posuere pharetra non blandit risus. Duis fringilla libero vitae nisl egestas elementum. Donec ut suscipit nunc, id imperdiet ipsum. Integer vel placerat felis, eget consectetur lorem. Nam mi purus, egestas sit amet ultrices venenatis, accumsan in purus. Nullam id leo vel enim aliquam congue. Morbi urna est, viverra vitae mattis suscipit, efficitur non est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
 
 const tokens = testText.split(" ");
-const wps = 100 / 60; //
+const wps = 100 / 60;
+
+interface TokenProps {
+  token: string;
+  highlightIndex: number;
+}
+
+function getHighlightIndex(token: string): number {
+  const mid = Math.floor(token.length / 2);
+  return (token.length % 2 !== 0) ? mid : mid - 1;
+}
 
 function SpeedReaderComponent() {
   const [currIndex, setCurrIndex] = useState<number>(0);
@@ -21,10 +32,23 @@ function SpeedReaderComponent() {
   return (
     <>
       <div className="token">
-        {tokens[currIndex]}
+        <TokenComponent token={tokens[currIndex]} highlightIndex={getHighlightIndex(tokens[currIndex])}></TokenComponent>
       </div>
     </>
   )
 }
+
+function TokenComponent({ token, highlightIndex }: TokenProps) {
+  return (<>
+    {
+      token.split("").map((char, index) => {
+        const style = (index === highlightIndex) ? 'token_hl' : '';
+
+        return <span key={index} className={style ?? undefined}>{char}</span>
+      })
+    }
+  </>)
+}
+
 
 export default SpeedReaderComponent;
