272f4c3ad7826fea2baaec17408ffe5361c2d3a4
Christina <christinaraganit@gmail.com>
2026-01-17 18:44:13 -0800

    created pages folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router';
+import UploadedFile from './pages/UploadedFile.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}></Route>
+        <Route path="/uploaded" element={<UploadedFile />}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- src/uploaded/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function UploadedApp() {
-    return (
-        <div className="grid grid-cols-6 min-h-screen min-w-screen">
-
-        </div>
-    )
-}
