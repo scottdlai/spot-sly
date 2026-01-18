5506c9194ef8d689a8c765ac93538ef1f18b5e52
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-01-17 20:37:34 -0800

    wip: add reader component to routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router';
+import SpeedReaderComponent from './reader/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}></Route>
+        <Route path="/reader" element={<SpeedReaderComponent />}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
